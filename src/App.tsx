5e858d7c17dfccbd7fb20beaae3c7e5a22f70506
Stephanie Oguocha <stephanieoguocha@gmail.com>
2026-01-13 20:34:58 +0100

    feat: add results screen design and functionality

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ import Passage from "./components/Passage";
 import passagesData from "./data/data.json";
 import { useTypingTest } from "./hooks/useTypingTest";
 import { calculateStats } from "./utils/calculateStats";
+import Results from "./components/Results";
 
 const data = passagesData as PassageData;
 export type ModeConfig =
@@ -48,6 +49,15 @@ function App() {
   }, [difficulty]);
 
   const displayTime = modeConfig.mode === "timed" ? timeLeft : elapsedTime;
+  // const isFinished =
+  //   (modeConfig.mode === "timed" && timeLeft === 0) ||
+  //   (modeConfig.mode === "passage" &&
+  //     typed.length > 0 &&
+  //     typed.length === passage?.text.length);
+
+  const isFinished =
+    timeLeft === 0 ||
+    (typed.length > 0 && typed.length === passage?.text.length);
 
   return (
     <>
@@ -61,16 +71,27 @@ function App() {
         modeConfig={modeConfig}
         setModeConfig={setModeConfig}
         onReset={resetGame}
-        isTesting=
-        {isTesting}
+        isTesting={isTesting}
       />
-      {passage && (
+      {passage && !isFinished && (
         <Passage
           text={passage.text}
           hasStarted={hasStarted}
           typed={typed}
           errors={errors}
           onStart={startGame}
+          onRestart={resetGame}
+        />
+      )}
+
+      {isFinished && (
+        <Results
+          wpm={stats.wpm}
+          accuracy={stats.accuracy}
+          characters={typed.length}
+          totalTyped={typed.length}
+          errorCount={errors.length}
+          bestWpm={92} // placeholder for now
           onRestart={resetGame}
         />
       )}
--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -1,7 +1,102 @@
-function Results() {
+import type React from "react";
+
+type ResultsProps = {
+  wpm: number;
+  accuracy: number;
+  characters: number;
+  totalTyped: number;
+  errorCount: number;
+  bestWpm: number;
+  onRestart: () => void;
+};
+
+export default function Results({
+  wpm,
+  accuracy,
+  characters,
+  totalTyped,
+  errorCount,
+  bestWpm,
+  onRestart,
+}: ResultsProps) {
   return (
-    <div>Results</div>
-  )
+    <section className="flex flex-col items-center text-center gap-6 mt-12">
+      <div className="w-full flex flex-col items-center justify-center">
+        <img
+          src="/images/icon-completed.svg"
+          alt="icon completed"
+          className="w-fit"
+        />
+        <img
+          src="/images/pattern-star-2.svg"
+          alt="restart icon"
+          className="self-start size-5"
+        />
+      </div>
+
+      {/* Heading */}
+      <h2 className="text-2xl font-semibold">Test Complete!</h2>
+      {/* Message */}
+      <p className="text-neutral-400">
+        Solid run. Keep pushing to beat your high score.
+      </p>
+
+      {/* Main result */}
+      <div className="flex flex-col gap-4 mt-4 w-full">
+        <ResultStat label="WPM:" value={wpm} />
+        <ResultStat
+          label="Accuracy:"
+          value={`${accuracy}%`}
+          isError={accuracy < 100}
+        />
+        {/* <ResultStat label="Characters" value={characters} /> */}
+        {/* <ResultStat label="Characters"  value={`${totalTyped}/${errorCount}`} /> */}
+        <ResultStat
+          label="Characters"
+          value={
+            <span className="flex items-center gap-1">
+              <span className="text-green-500">{totalTyped}</span>
+              <span className="text-neutral-500">/</span>
+              <span className="text-red-500">{errorCount}</span>
+            </span>
+          }
+        />
+      </div>
+
+      {/* Action */}
+      <button
+        onClick={onRestart}
+        className="mt-4 px-6 py-2 rounded-xl bg-neutral-0 text-neutral-900 font-semibold hover:bg-blue-500 transition"
+      >
+        Go again
+        {/* <img src="/images/icon-restart.svg" alt="restart icon" /> */}
+      </button>
+      <img
+        src="/images/pattern-star-1.svg"
+        alt="restart icon"
+        className="self-end size-10"
+      />
+    </section>
+  );
 }
 
-export default Results
+type ResultStatProps = {
+  label: string;
+  value: string | number | React.ReactNode;
+  isError?: boolean;
+};
+
+function ResultStat({ label, value, isError = false }: ResultStatProps) {
+  return (
+    <div className="flex flex-col gap-3 border border-neutral-700 w-full p-4 rounded-lg text-left">
+      <p className="text-neutral-400 text-lg">{label}</p>
+      <p
+        className={`text-2xl font-bold ${
+          isError ? "text-red-500" : "text-neutral-0"
+        }`}
+      >
+        {value}
+      </p>
+    </div>
+  );
+}
